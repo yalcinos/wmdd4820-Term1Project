6d88d464f18e074230c4afac2d57ecf222d6f13f
yalcinos <yalcintatar41@gmail.com>
2019-07-25 02:03:14 -0700

    Bug:Old data shows on the pie

--- standings.js
+++ standings.js
@@ -5,6 +5,8 @@
  * 
  * 
  */
+function onLoadFetch(){
+
 var getStandings = new Request('https://api-football-v1.p.rapidapi.com/v2/leagueTable/114 ', {
 	headers: new Headers({
 		'Content-Type': 'text/plain',
@@ -23,6 +25,7 @@ fetch(getStandings)
   console.log(responseAsJson);
   console.log(responseAsJson.api.standings[0][0].all);
 
+  //Assign values to the combobox
   for(let i = 0 ; i<responseAsJson.api.standings[0].length; i++){
     let cmbbox = document.getElementById("combobox-team");
     let optionitem = document.createElement("option");
@@ -30,57 +33,97 @@ fetch(getStandings)
     optionitem.value = responseAsJson.api.standings[0][i].teamName;
     cmbbox.appendChild(optionitem);
   }
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = document.getElementById('myChart');
-  chart.style.display='block';
-  var doughnutChart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'doughnut',
-
-    // The data for our dataset
-    data: {
-        labels: Object.keys(responseAsJson.api.standings[0][0].home),       
-        datasets: [{
-            backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
-            borderColor: 'rgb(0, 0, 0)',
-            data: Object.values(responseAsJson.api.standings[0][0].home)
-        }]
-    },
-
-    // Configuration options go here
-    options: {
-      responsive :true
-    }
+  
 });
-//Away Statistic
-var awayChart = document.getElementById('myChart1').getContext('2d');
-var chart = document.getElementById('myChart1');
-var doughnutChart = new Chart(awayChart, {
-  // The type of chart we want to create
-  type: 'doughnut',
-
-  // The data for our dataset
-  data: {
-      labels: Object.keys(responseAsJson.api.standings[0][0].away),       
-      datasets: [{
-          backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
-          borderColor: 'rgb(0, 0, 0)',
-          data: Object.values(responseAsJson.api.standings[0][0].away)
-      }]
-  },
-
-  // Configuration options go here
-  options: {
-    responsive :true
-  }
-});
-
-});
+}
  /*
  *
  *End of Transfer Part
  *  
  */
+function changeDataByTeam(){
+
+  var getStandings = new Request('https://api-football-v1.p.rapidapi.com/v2/leagueTable/114 ', {
+    headers: new Headers({
+      'Content-Type': 'text/plain',
+          'X-RapidAPI-Key':'dc03adc6e7mshf1c57d344b8843ep16cc1fjsnca50c7807d33'
+    })
+  });
+  fetch(getStandings)
+    .then(function(response) {
+      if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    // Read the response as json.
+    return response.json();
+  })
+  .then(function(responseAsJson) {
+    return responseAsJson;
+  }).then((getResponse) => getDataByTeam(getResponse));
+  }
+
+
+ function getDataByTeam(event){
+     //Assign values to the combobox
+  
+    let cmbboxIndex = document.getElementById("combobox-team").selectedIndex;
+    console.log(cmbboxIndex);
+    console.log(Object.values(event.api.standings[0][cmbboxIndex].home));
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart1 = document.getElementById('myChart');
+    chart1.style.display='block';
+    
+    if(doughnutChart != undefined){
+      doughnutChart.destroy();
+    }
+      
+    var doughnutChart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'doughnut',
+  
+      // The data for our dataset
+      data: {
+          labels: Object.keys(event.api.standings[0][cmbboxIndex].home),       
+          datasets: [{
+              backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
+              borderColor: 'rgb(0, 0, 0)',
+              data: Object.values(event.api.standings[0][cmbboxIndex].home)
+          }]
+      },
+  
+      // Configuration options go here
+      options: {
+        responsive :true
+      }
+  });
+  
+  //doughnutChart.destroy();
+  //Away Statistic
+  var awayChart = document.getElementById('myChart1').getContext('2d');
+  var chart2= document.getElementById('myChart1');
+  var doughnutChart1 = new Chart(awayChart, {
+    // The type of chart we want to create
+    type: 'doughnut',
+  
+    // The data for our dataset
+    data: {
+        labels: Object.keys(event.api.standings[0][cmbboxIndex].away),       
+        datasets: [{
+            backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
+            borderColor: 'rgb(0, 0, 0)',
+            data: Object.values(event.api.standings[0][cmbboxIndex].away)
+        }]
+    },
+  
+    // Configuration options go here
+    options: {
+      responsive :true
+    }
+    
+  });
+  
+ }
+
 
 
 
