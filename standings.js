73702939b5a3fd56070bf09d5e918c8d52cf4e9c
yalcinos <yalcintatar41@gmail.com>
2019-07-24 23:52:42 -0700

    Feat: Add style of graphs and add combobox and give values from api

--- standings.js
+++ standings.js
@@ -23,8 +23,16 @@ fetch(getStandings)
   console.log(responseAsJson);
   console.log(responseAsJson.api.standings[0][0].all);
 
+  for(let i = 0 ; i<responseAsJson.api.standings[0].length; i++){
+    let cmbbox = document.getElementById("combobox-team");
+    let optionitem = document.createElement("option");
+    optionitem.innerHTML = responseAsJson.api.standings[0][i].teamName;
+    optionitem.value = responseAsJson.api.standings[0][i].teamName;
+    cmbbox.appendChild(optionitem);
+  }
   var ctx = document.getElementById('myChart').getContext('2d');
   var chart = document.getElementById('myChart');
+  chart.style.display='block';
   var doughnutChart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'doughnut',
@@ -40,7 +48,31 @@ fetch(getStandings)
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+      responsive :true
+    }
+});
+//Away Statistic
+var awayChart = document.getElementById('myChart1').getContext('2d');
+var chart = document.getElementById('myChart1');
+var doughnutChart = new Chart(awayChart, {
+  // The type of chart we want to create
+  type: 'doughnut',
+
+  // The data for our dataset
+  data: {
+      labels: Object.keys(responseAsJson.api.standings[0][0].away),       
+      datasets: [{
+          backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
+          borderColor: 'rgb(0, 0, 0)',
+          data: Object.values(responseAsJson.api.standings[0][0].away)
+      }]
+  },
+
+  // Configuration options go here
+  options: {
+    responsive :true
+  }
 });
 
 });
