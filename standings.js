75d28365b1f76072d321af821ce2a84d79612d70
yalcinos <yalcintatar41@gmail.com>
2019-07-28 12:50:00 -0700

    Feat: Add default graph when first visit on standings.

--- standings.js
+++ standings.js
@@ -33,8 +33,9 @@ fetch(getStandings)
     optionitem.value = responseAsJson.api.standings[0][i].teamName;
     cmbbox.appendChild(optionitem);
   }
+    return responseAsJson;
+}).then((getResponse1) => onLoadFetchFirstTeamData(getResponse1));
   
-});
 }
  /*
  *
@@ -68,7 +69,7 @@ function changeDataByTeam(){
   
     let cmbboxIndex = document.getElementById("combobox-team").selectedIndex;
     console.log(cmbboxIndex);
-    console.log(Object.values(event.api.standings[0][cmbboxIndex].home));
+    console.log(event.api.standings[0][0].home);
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart1 = document.getElementById('myChart');
     chart1.style.display='block';
@@ -97,7 +98,7 @@ function changeDataByTeam(){
       }
   });
   
-  //doughnutChart.destroy();
+  
   //Away Statistic
   var awayChart = document.getElementById('myChart1').getContext('2d');
   var chart2= document.getElementById('myChart1');
@@ -125,9 +126,66 @@ function changeDataByTeam(){
     }
     
   });
-  
+    return event;
  }
- function onLoadFetchGraph(){
+ function onLoadFetchFirstTeamData(event){
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart1 = document.getElementById('myChart');
+    chart1.style.display='block';
+    //Below code prevents to destroy old graph, otherwise when choose  teams from dropbox, graph is not overwrited and shows every team data.
+ 
+    
+    window.doughnut = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: Object.keys(event.api.standings[0][0].home),       
+        datasets: [{
+            backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
+            borderColor: 'rgb(0, 0, 0)',
+            data: Object.values(event.api.standings[0][0].home)
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+      responsive :true
+    }
+    });
+
+
+    //Away Statistic
+  var awayChart = document.getElementById('myChart1').getContext('2d');
+  var chart2= document.getElementById('myChart1');
+
+  if(window.doughnutaway != undefined){
+    window.doughnutaway.destroy();
+  }
+   window.doughnutaway = new Chart(awayChart, {
+    // The type of chart we want to create
+    type: 'doughnut',
+  
+    // The data for our dataset
+    data: {
+        labels: Object.keys(event.api.standings[0][0].away),       
+        datasets: [{
+            backgroundColor: ['rgb(139,0,0)','rgb(0,128,0)','rgb(0,0,205)','rgb(255,0,0)','rgb(255,218,185)','rgb(47,79,79)'],
+            borderColor: 'rgb(0, 0, 0)',
+            data: Object.values(event.api.standings[0][0].away)
+        }]
+    },
+  
+    // Configuration options go here
+    options: {
+      responsive :true
+    }
+    
+  });
+    
+    
 
  }
 
